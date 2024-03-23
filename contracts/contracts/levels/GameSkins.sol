a058d879281cd6ed2591c0978e3023be3aa52dcc
Thigape <thigalante@gmail.com>
2024-03-23 11:53:13 -0300

    Adding GameSkins.sol level

---
+++ contracts/contracts/levels/GameSkins.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract GameSkins {
+    ERC721 ourNFT
+
+    constructor(ERC721 nft) {
+        ourNFT = nft;
+    }
+    
+    function BuySkin {
+        
+    }
+}
